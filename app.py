2026feccff5989e2632b020483c25553de9627ca
vajrapumanikanta-dev <manivajrapu360@gmail.com>
2026-09-13 08:17:27 +0000

    Prepare Smart Expense Tracker for Docker deployment

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def create_app(config_name=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
